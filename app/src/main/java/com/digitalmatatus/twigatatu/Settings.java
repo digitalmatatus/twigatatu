c2ad29f938e87cfbde54b228149ccfbd43260d01
ratemoh <rstephenosoro@gmail.com>
2018-05-28 00:35:25 +0300

    Added Settings checkbox

--- app/src/main/java/com/digitalmatatus/twigatatu/Settings.java
+++ app/src/main/java/com/digitalmatatus/twigatatu/Settings.java
@@ -5,7 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import net.igenius.customcheckbox.CustomCheckBox;
 
 public class Settings extends AppCompatActivity {
 
@@ -15,6 +18,15 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        final CustomCheckBox scb = (CustomCheckBox) findViewById(R.id.scb);
+        scb.setOnCheckedChangeListener(new CustomCheckBox.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CustomCheckBox checkBox, boolean isChecked) {
+                Log.d("CustomCheckBox", String.valueOf(isChecked));
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
